b310e4c9ca3d1ce14a2e96c4ab4e14e0358efef8
Rajeev Kumar <rajeevkumar@Rajeevs-MacBook-Air.local>
2025-11-13 12:23:07 +0530

    fibonacci program

---
+++ Dynamic Programmig/fibonacci.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define f(i,a,b) for(int i=a;i<b;i++)
+#define ll long long
+#define nl "\n"
+#define vi vector<ll>
+
+
+
+//Memoization approach
+// int n;
+// vi dp;
+
+// int fib(int n){
+//     //base case
+//     if(n==1) return 1;
+//     if(n==0) return 0; 
+//     //memoization.
+//     if(dp[n]!=-1) return dp[n];
+
+//     return dp[n] = fib(n-1) + 1ll +  fib(n-2);
+// }
+
+// void solve(){
+//     cin>>n;
+//     dp.assign(n+1, -1);
+//     fib(n);
+//     cout<<dp[n]<<nl;
+// }
+
+//bottom up approach
+void solve(){
+    int n; cin>>n;
+    vi dp(n+1);
+    dp[1]=1;
+    for(int i=2;i<=n;i++){
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+    cout<<dp[n]<<nl;
+}
+
+int main(){
+    auto start = chrono :: high_resolution_clock::now();
+    int t=1; 
+    //cin>>t;
+    while(t--){
+        solve();
+    }
+
+    auto end = chrono :: high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
+    cout << "Execution time: " << duration.count() << " ms" << nl;
+
+}
